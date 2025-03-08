871c5d95b15d53d137eaf5546c47c4318bd52f65
Sophia Lin <50805131+sophialin7@users.noreply.github.com>
2025-03-07 18:53:43 -0800

    Update background.js

--- background.js
+++ background.js
@@ -1,17 +1,3 @@
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.contextMenus.create({
-        id: "extractSubtitles",
-        title: "Extract YouTube Subtitles",
-        contexts: ["page"],
-        documentUrlPatterns: ["https://www.youtube.com/watch*"]
-    });
-});
-
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-    if (info.menuItemId === "extractSubtitles") {
-        chrome.scripting.executeScript({
-            target: { tabId: tab.id },
-            function: extractSubtitles
-        });
-    }
+    console.log("YouTube Metadata Extension Installed");
 });
